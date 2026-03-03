d568c0fb9ff5dfb11dfc2eb06e81a6e1c8c37356
norshaqina83-cpu <norshaqina83@gmail.com>
2026-03-03 04:45:29 +0000

    logo in middle

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -45,10 +45,10 @@ export default function Navbar() {
   return (
     <nav className="bg-slate-900 border-b border-slate-700 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-center h-screen">
           {/* Logo */}
           <Link href={user ? (user.role === "parent" ? "/parent" : user.role === "teacher" ? "/teacher" : "/") : "/login"} className="flex items-center gap-2">
-            <img src="/workspaces/AutoMark/public/logo.png" alt="RFID Attendance Logo" className="w-8 h-8 rounded-lg flex items-center justify-center text-white font-bold text-sm" />
+            <img src="/logo.png" alt="RFID Attendance Logo" className="w-8 h-8 rounded-lg flex items-center justify-center text-white font-bold text-sm" />
             <span className="text-white font-semibold text-lg hidden sm:block">
               AutoMark
             </span>
